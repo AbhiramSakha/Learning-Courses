a93082d422bab5a3d582316efe2f4fb9e182dc1d
AbhiramSakha <143825001+AbhiramSakha@users.noreply.github.com>
2025-07-08 15:11:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -170,9 +170,7 @@ def get_youtube_playlist(course):
         print(f"Response content: {response.text}")
         return None
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
